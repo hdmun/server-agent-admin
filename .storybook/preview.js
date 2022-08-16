77d241893d76839acfa48082f24a7563f3d7b525
hdmun <hdmun@kakao.com>
2022-08-16 19:11:49 +0900

    `@nuxtjs/storybook`에 필요없는 파일들이다

--- .storybook/preview.js
+++
@@ -1,9 +0,0 @@
-import '~~/.nuxt-storybook/storybook/preview.js'
-import {VApp} from 'vuetify/lib'
-
-export const decorators = [() => {
-  return {
-    template: "<v-app><story/></v-app>",
-    components: {VApp}
-  }
-}]
